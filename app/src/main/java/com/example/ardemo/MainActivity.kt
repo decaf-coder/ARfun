4aa62246b58c0b5fd1a6861fda09bb25458dcbe2
Serena Drouillard <serenadrouillard@gmail.com>
2019-10-17 14:57:23 -0300

    first

---
+++ app/src/main/java/com/example/ardemo/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.ardemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.ar.sceneform.ux.ArFragment
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
